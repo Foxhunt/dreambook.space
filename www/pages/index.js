a2ca12ffa31f63c141179ae6a4c56a8feec2db67
Marco <marco.mojica@hotmail.de>
2019-01-31 01:07:22 +0800

    Add text field for newDream

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,15 +1,30 @@
 import "isomorphic-unfetch"
 
+const handleSubmit = event => {
+    event.preventDefault()
+    const form = new FormData(event.target)
+    const text = form.get("dream")
+
+    fetch('/api/newDream', {
+        method: 'POST',
+        body: { text },
+    })
+}
+
 const Page = ({ dreams }) =>
     <>
         {dreams.map(({ text, _id }) => <div key={_id}>{ text }</div>)}
+        <form onSubmit={ handleSubmit }>
+            <input name="dream" type="text" />
+        </form>
     </>
 
 Page.getInitialProps = async ({ req }) => {
     const url = req ? `https://${req.headers.host}/api/getDreams` : "/api/getDreams"
     const res = await fetch(url)
     const dreams = await res.json()
-    return { dreams }
+   
+   return { dreams }
 }
 
 export default Page
