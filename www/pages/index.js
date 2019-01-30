0f79f0b4bba9f83fee58b8b9012db9aeab60a7a0
Marco <marco.mojica@hotmail.de>
2019-01-30 15:20:03 +0100

    Add dream fetching to index

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,1 +1,15 @@
-export default () => "dreams go here"
+import "isomorphic-unfetch"
+
+const Page = ({ dreams }) =>
+    <>
+        {dreams.map(({ text }, i) => <div key={i}>{ text }</div>)}
+    </>
+
+Page.getInitialProps = async ({ req }) => {
+    const res = await fetch("/api/getDreams")
+    const dreams = await res.json()
+    console.log(dreams)
+    return { dreams }
+}
+
+export default Page
