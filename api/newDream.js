c41e5aeca9fd21680f565c34212312fad61c73a2
Marco <marco.mojica@hotmail.de>
2019-01-31 18:32:42 +0800

    Fix typo add error handling

--- api/newDream.js
+++ api/newDream.js
@@ -3,13 +3,17 @@ const { json, send } = require("micro")
 
 module.exports = async (req, res) => {
     try {
-        const [ db, { text } ] = await Promisse.all([mongo(), json(req)])
+        const [ db, { text } ] = await Promise.all([mongo(), json(req)])
     
         const col = db.collection("dreams")
     
         const insertCount = await col.insertOne({ text })
 
-        send(res, 200, "Dream was added")    
+        if (insertCount === 1) {
+            send(res, 200, "Dream was added")   
+        } else {
+            throw new Error("could not insert Dream!")
+        }
     } catch(error) {
         console.error(error)
         send(res, 500, "error")
